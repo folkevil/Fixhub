637148466f6cdf8812049a6a4031d0a6cb720069
phecho <phecho@phecho.com>
2016-09-03 12:18:42 +0800

    Add settings stuff

---
+++ resources/lang/zh-CN/settings.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of Fixhub.
+ *
+ * Copyright (C) 2016 Fixhub.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    'label'        => '系统设置',
+    'setup'        => '常规设置',
+    'localization' => '本地化',
+    'url'          => 'URL',
+    'language'     => '语言',
+    'show_support' => '是否展示支持Fixhub?',
+
+];
--- resources/views/dashboard/_partials/nav.blade.php
+++ resources/views/dashboard/_partials/nav.blade.php
@@ -73,6 +73,9 @@
                         </li>
                     </ul>
                 </li>
+                @if($current_user->is_admin)
+                <li><a href="/admin/settings"><i class="ion ion-wrench"></i> {{ trans('settings.label') }}</a></li>
+                @endif
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="{{ $current_user->avatar_url }}" class="user-image" />
